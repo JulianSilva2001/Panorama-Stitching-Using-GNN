881ab4034ec376a69cb91c1d73206567c6119412
JulianSilva2001 <sackjuliansilva@gmail.com>
2024-10-16 16:31:15 +0530

    Initial commit

---
+++ SuperGluePretrainedNetwork-master/load_npz.py
@@ -0,0 +1,5 @@
+import numpy as np
+path = 'F:\SuperGluePretrainedNetwork-master\SuperGluePretrainedNetwork-master\dump_match_pairs\scene0711_00_frame-001680_scene0711_00_frame-001995_matches.npz'
+npz = np.load(path)
+print(npz.files)
+
